9af17aa7fae79c6542adc7adc357007c58fa3d11
Alex <awarrington0895@gmail.com>
2023-05-18 12:26:08 +0000

    Complete rna_transcription exercise

--- go/rna-transcription/rna_transcription.go
+++ go/rna-transcription/rna_transcription.go
@@ -1,5 +1,26 @@
 package strand
 
+func complement(dnaNucleotide rune) rune {
+	switch dnaNucleotide {
+	case 'G':
+		return 'C'
+	case 'C':
+		return 'G'
+	case 'T':
+		return 'A'
+	case 'A':
+		return 'U'
+	default:
+		return 'A'
+	}
+}
+
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	var rna []rune
+
+	for _, nucleotide := range dna {
+		rna = append(rna, complement(nucleotide))
+	}
+
+	return string(rna)
 }
